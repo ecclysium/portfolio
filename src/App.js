1242a97c0191d822427fffff995b57212f47a122
Gray <simpson.gray.01@gmail.com>
2024-03-25 17:39:28 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. Or don't, I don't know, you do you. Bitch.
         </p>
         <a
           className="App-link"
